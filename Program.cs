0560fdadbf59b2b1fb5acf5f1bff3025fdf4c615
Frozin-Crizor <95418865+Frozin-Crizor@users.noreply.github.com>
2021-12-02 15:09:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ya!");
+            string name="Andrey";
         }
     }
 }
